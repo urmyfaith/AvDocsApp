883223d65f50a22b7539d4177164f9b29fcdac5c
Sakib Mulla <mr.sakibmulla@gmail.com>
2020-04-25 00:45:38 +0530

    commit

--- model/ClinicMaster.go
+++ model/ClinicMaster.go
@@ -5,11 +5,18 @@ import "time"
 type ClinicMaster struct {
 	ClinicId    int64     `json:"clinicId" validate:"required"`
 	ClinicName  string    `json:"clinicName" validate:"required"`
-	ContactNo   int32     `json:"contactNo" validate:"required"`
-	Address     string    `json:"address" validate:""`
+	ContactNo   []ClinicContact     `json:"contactNo" `
+	Cityname   	string	  `json:"cityname" validate:"required"`
+	Address     string    `json:"address"`
 	Pincode     int       `json:"pincode" validate:"required"`
 	District    string    `json:"district" validate:"required"`
-	Country     string    `json:"country" validate:""`
-	CreatedDate time.Time `json:"country"`
+	Country     string    `json:"country"`
+	CreatedDate time.Time `json:"createdDate"`
 	CreatedBy   int64     `json:"createdBy" validate:"required"`
+}
+
+type ClinicContact struct {
+	ClinicId    int64     `json:"clinicId" validate:"required"`
+	ClinicName  string    `json:"clinicName" validate:"required"`
+	ContactNo   int64     `json:"contactNo" validate:"required"`
 }
